630ba4e14325b9fe5a1327840b5506a0968e0d41
Andrey Soroko <andreysoroko84@gmail.com>
2023-01-16 12:39:39 +0300

    init: start reverse int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
+    if (n < 0) n = n * -1;
+    
+  n = n.toString();
+  straight_array = n.split('');
   
+  let j = straight_array.length;
+    let reverse_array = [];
+
+    for (let i = 0; i < straight_array.length; i++) {
+    reverse_array[i] = straight_array[j-1];
+    j--;
+  }
+
+  reverse_array = reverse_array.join('');
+
+  return Number(reverse_array);
+
 }
